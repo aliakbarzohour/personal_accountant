78e91a8e80739b4ae0bbcf6e26a0ef2672631d9a
aliakbarzohour <aliakbar.zohour@gmail.com>
2023-03-14 22:09:11 +0330

    income model and save one income money

--- backend/models.py
+++ backend/models.py
@@ -10,3 +10,9 @@ class Expense(models.Model):
     amount = models.BigIntegerField()
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
+
+class Income(models.Model):
+    text = models.CharField(max_length=255)
+    date = models.DateTimeField()
+    amount = models.BigIntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
