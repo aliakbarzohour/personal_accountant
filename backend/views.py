8d93aef9ffebcd6edb83937c3c5fd35f4ecd03f9
aliakbarzohour <aliakbar.zohour@gmail.com>
2023-03-16 15:14:08 +0330

    add csrf exemt for post methods

--- backend/views.py
+++ backend/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 import json
+from django.views.decorators.csrf import csrf_exempt
 # Create your views here.
 
+
+@csrf_exempt
 def submit_expenes(request):
     return render(request, 'home.html')
 
