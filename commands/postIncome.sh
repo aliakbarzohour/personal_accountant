228e3858b8c0d6d8968e5d012435d1707be06040
aliakbarzohour <aliakbar.zohour@gmail.com>
2023-03-19 01:52:15 +0330

    change bash

--- commands/postIncome.sh
+++ commands/postIncome.sh
@@ -2,4 +2,4 @@
 
 token=12345678
 
-curl --data "token=$token&amount=$1&text=$2" http://localhost:8008/submit/income/ 
+curl --data "token=$token&amount=$1&text=$2" http://localhost:8008/submit/incomes/ 
