9cbfe2383b9fd7def92930b1ec7bbded881882a9
aliakbarzohour <aliakbar.zohour@gmail.com>
2023-03-19 01:49:44 +0330

    new url

--- backend/urls.py
+++ backend/urls.py
@@ -2,8 +2,7 @@ from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
-    # path('', views.submit_expenes),
     path('', views.Home, name='home'),
-    re_path(r'^submit/expense/$', views.submit_expenes, name='submit_expense')
-    re_path(r'^submit/incomes/$', views.submit_incomes, name='submit_income')
+    re_path(r'^submit/expense/$', views.submit_expenes, name='submit_expense'),
+    re_path(r'^submit/incomes/$', views.submit_incomes, name='submit_income'),
 ]
