b0e3cb775e4f6efe13d35b9413760796f562d458
aliakbarzohour <aliakbar.zohour@gmail.com>
2023-03-19 00:23:05 +0330

    fix model

--- backend/models.py
+++ backend/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 
 class Token(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    token = models.CharField(max_length=48)
+    token = models.CharField(max_length=255)
     def __str__(self):
         return "{}-token".format(self.user)
 
