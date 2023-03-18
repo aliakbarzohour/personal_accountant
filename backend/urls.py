76df935c6c0d5762204c260b8a93587e72795e1e
aliakbarzohour <aliakbar.zohour@gmail.com>
2023-03-19 00:37:32 +0330

    home url

--- backend/urls.py
+++ backend/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     # path('', views.submit_expenes),
+    path('', views.Home, name='home'),
     re_path(r'^submit/expense/$', views.submit_expenes, name='submit_expense')
 ]
